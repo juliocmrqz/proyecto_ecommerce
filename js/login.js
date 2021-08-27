0ee405b49d97aed69c83e65e245c63e56740a4d0
juliocmrqz <juliomarquez3093@gmail.com>
2021-08-27 00:18:34 -0300

    agregado onLoad y los botones para redireccionar

--- js/login.js
+++ js/login.js
@@ -8,15 +8,22 @@ function onSignIn(googleUser) {
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    // window.location.href = "home.html";
+    window.location.href = "home.html";
 }
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
-    });
+    })
+    window.location.href = "index.html";
 }
+
+function onLoad() {
+    gapi.load('auth2', function () {
+    gapi.auth2.init();
+   });
+ }
 
 document.addEventListener("DOMContentLoaded", function (e) {
 
