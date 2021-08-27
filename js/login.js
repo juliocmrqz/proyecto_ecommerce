4248683befd64e3fdc30c8c62ac55f2b33692cd5
juliocmrqz <juliomarquez3093@gmail.com>
2021-08-27 00:13:17 -0300

    agregado Login Google

--- js/login.js
+++ js/login.js
@@ -1,6 +1,23 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    // window.location.href = "home.html";
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
 
 });
