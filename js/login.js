826c98981276718edb713b9f51f5594d12eeea15
juliocmrqz <juliomarquez3093@gmail.com>
2021-09-01 12:51:29 -0300

    Agregado el User Local

--- js/login.js
+++ js/login.js
@@ -1,7 +1,27 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+/**
+ * Se crean las constantes necesarias para traerme los datos el formulario y sus datos
+ * No utilizo preventDefault en el eventListener ya que me evita la ejecución del action del formulario
+ * No tengo que utilizar location.href porque tengo un action al submit
+ */
+const formularioInicioSesion = document.getElementById("formularioInicioSesion");
+const usuarioInicioSesion = document.getElementById("usuarioInicioSesion");
+const passwordInicioSesion = document.getElementById("passwordInicioSesion");
 
+formularioInicioSesion.addEventListener('submit', function (event) {
+    let usuariosIniciados = Array({
+        usuario: usuarioInicioSesion.value,
+        password: passwordInicioSesion.value,
+    })
+    localStorage.setItem('usuario', JSON.stringify(usuariosIniciados));
+});
+
+
+/**
+ * USO FUNCION DE GOOGLE PARA INICIO DE SESIÓN
+ * PENSAR EN UNA FORMA DE OBTENER LOS DATOS PARA PODER ALMACENARLOS Y MOSTRARLOS EN EL MISMO LOCALSTORAGE SI INICIARA
+ * SESIÓN CON EL FORMULARIO
+ * RECORDAR QUE SIEMPRE QUE QUIERA TRABAJAR EN SERVIDOR LOCAL ME VA A SALTAR ERROR DE OBTENCIÓN DE CREDENCIALES.
+ */
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
@@ -11,6 +31,8 @@ function onSignIn(googleUser) {
     window.location.href = "home.html";
 }
 
+/* 
+anexadas al userlocal.js
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
@@ -19,12 +41,13 @@ function signOut() {
     })
     window.location.href = "index.html";
 }
-
 function onLoad() {
     gapi.load('auth2', function () {
         gapi.auth2.init();
     });
 }
+*/
 
-document.addEventListener("DOMContentLoaded", function (e) {
-});
+/* No utilizo el DOMContentLoaded ya que tengo el script ejecutando al final de la página y son todos eventos externos. 
+document.addEventListener("DOMContentLoaded", function (e) {});
+*/
---
+++ js/userlocal.js
@@ -0,0 +1,42 @@
+// obtengo el div donde voy a agregar los datos y funcionalidades
+const userlocal = document.getElementById("userlocal");
+
+// función para capitalizar una palabra
+function capitalize(word) {
+    const lower = word.toLowerCase();
+    return word.charAt(0).toUpperCase() + lower.slice(1);
+}
+
+// Tomo los datos que instancié en usuario dentro de login.js
+let usuarioInicioSesion = JSON.parse(localStorage.getItem('usuario'));
+
+// Considero de no ser null el usuarioInicioSesion insertar el elemento solo considerando el usuario con sus funcionalidades
+if (usuarioInicioSesion != null) {
+    userlocal.innerHTML += `<a id="nombreUsuario" class="py-2 d-none d-md-inline-block cerrarsesion" href="#" onclick="signOut();">${capitalize(usuarioInicioSesion[0].usuario)}</a>`
+} else { // agrego la excepción si no existe nada me libere el boton Login
+    userlocal.innerHTML += `<a class="py-2 d-none d-md-inline-block" href="index.html">Login</a>`
+}
+// Si quiero obtener los usuarios que está almacenando la página anterior en consola
+// console.log(usuarioInicioSesion);
+
+
+/**
+ * TO DO
+ * Agregar los datos del usuario y probar si funciona todo ok con Google
+ */
+
+
+// Función de cierre de sesión sea con Google o normalmente para redirigir al Login page
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    })
+    location.href = "index.html";
+  }
+
+  function onLoad() {
+    gapi.load('auth2', function () {
+      gapi.auth2.init();
+    });
+  }
