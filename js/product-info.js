6cf548ebce1d299aaa328614d255bd25deea68a5
juliocmrqz <juliomarquez3093@gmail.com>
2021-09-18 13:02:17 -0300

    Agregado control para comentarios
    Se agregaron los formatos necesarios para tener un controlador de comentarios con estrellas que tome esos valores y los devuelva al HTML.

--- js/product-info.js
+++ js/product-info.js
@@ -1,24 +1,43 @@
-let currentProduct = {};
 const PRODUCT_INFORMATION_TITLE = document.getElementById("product-info-title");
 const PRODUCT_INFO_CONTAINER = document.getElementById("product-info-container");
 const PRODUCT_INFO = document.getElementById("product-info");
 const PRODUCT_INFO_DISPLAY = document.getElementById("product-info-display");
-const MAIN_IMAGE_CONTAINER = document.getElementById("main-image-container");
-const MAIN_IMAGE = document.getElementById("main-image");
+const IMAGE_CONTAINER = document.getElementById("main-image-container");
 const PRODUCT_NAME = document.getElementById("product-name");
 const PRODUCT_CATEGORY = document.getElementById("product-info-category");
 const PRODUCTS_INFO_URL = document.createElement("a");
+const COMENTARIOS = document.getElementById("comentarios-container");
+const PRODUCT_INFO_PRICE = document.getElementById("product-info-price");
+const FORMULARIO_COMENTARIOS = document.getElementById("formulario-comentarios");
+let currentProduct = [];
+let comentariosLista = [];
 
-
-
-// https://www.youtube.com/watch?v=ENyk_W-Eleo continuar con esa idea de presentación
-
+// Funciones para mostrar las imagenes de los productos en un carousel hecho con bootstrap
 function ShowProductInfo(productInfo) {
   if (productInfo != undefined) {
     currentProduct = productInfo
   }
-  console.log(currentProduct);
   document.title = `eMercado - ${currentProduct.name}`;
+
+  /*
+  {
+    "name": "Chevrolet Onix Joy",
+    "description": "Potenciá tu actitud con Onix Joy que, además de destacarse por su diseño juvenil y moderno, te ofrecé una óptima autonomía, un desempeño equilibrado y el máximo confort interior. \u003cbr\u003eYa sea un viaje largo o un simple paseo por la ciudad, el confort es uno de los puntos fuertes del Onix. Esta versión incluye aire acondicionado, asientos tapizados en tela y gran espacio interior que te garantiza el máximo confort.",
+    "cost": 13500,
+    "currency": "USD",
+    "soldCount": 14,
+    "category": "Autos",
+    "images": [
+        "img/prod1.jpg",
+        "img/prod1_1.jpg",
+        "img/prod1_2.jpg",
+        "img/prod1_3.jpg",
+        "img/prod1_4.jpg"
+    ],
+    "relatedProducts": [1, 3]
+}
+   */
+
 
   // titulo de la página de producto
   PRODUCT_INFORMATION_TITLE.setAttribute("class", "mt-5 mb-5 text-center font-weight-bolder");
@@ -27,11 +46,8 @@ function ShowProductInfo(productInfo) {
   PRODUCT_INFO_CONTAINER.setAttribute("class", "small-container");
   PRODUCT_INFO.setAttribute("class", "row");
 
-  // contenedor de la imagen principal del producto
-  MAIN_IMAGE_CONTAINER.setAttribute("class", "text-center col-md-6");
-  MAIN_IMAGE.setAttribute("src", currentProduct.images[0]);
-  MAIN_IMAGE.setAttribute("class", "w-100");
-  MAIN_IMAGE.setAttribute("alt", currentProduct.name);
+  // contenedor de las imagenes del producto
+  IMAGE_CONTAINER.setAttribute("class", "text-center col-md-6");
 
   PRODUCT_INFO_DISPLAY.setAttribute("class", "col-md-6")
   PRODUCT_NAME.setAttribute("class", "font-weight-bold w-100");
@@ -41,94 +57,109 @@ function ShowProductInfo(productInfo) {
   PRODUCTS_INFO_URL.href = "products.html";
   PRODUCTS_INFO_URL.innerHTML = currentProduct.category;
 
+  PRODUCT_INFO_PRICE.setAttribute("class", "w-100");
+  PRODUCT_INFO_PRICE.innerHTML = `${currentProduct.currency} ${currentProduct.cost}`;
+
+
   PRODUCT_CATEGORY.appendChild(PRODUCTS_INFO_URL);
 
+  IMAGE_CONTAINER.innerHTML = `
+  <div id="carouselProducto" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner"></div>
+  </div>`
 
+  let carouselContainer = document.getElementsByClassName("carousel-inner")[0]
+  for (let i = 0; i < currentProduct.images.length; i++) {
+    const imagen = currentProduct.images[i];
+    carouselContainer.innerHTML += `
+    <div class="carousel-item">
+      <img src="${imagen}" class="d-block w-100" alt="...">
+    </div>`
 
-
-
-
-
-
-
-  // PRODUCT_CONTAINER.innerHTML = `
-  //           <div class="row">
-
-
-  //           <div class="col-md-6">
-  //             <div>
-  //               <h2 class="font-weight-bold w-100">${currentProduct.name}</h1>
-  //               <p class="text-muted">Categoría: <a href="products.html">${currentProduct.category}</a></p>
-  //             </div>
-  //             <h3 class"w-100">Precio: ${currentProduct.currency} ${currentProduct.cost}</h3>
-  //             <div class="row">
-  //               <small class="text-muted col-md-12">${currentProduct.soldCount} artículos vendidos</small>
-  //             </div>
-  //             <div class="row">
-  //             </div>
-  //               <p class="mt-2 mb-0 font-weight-bold">Descripción</p>
-  //               <p class="">${currentProduct.description}</p>
-  //             </div>
-  //           </div>`
-
-  // for (const imagen of currentProduct.images) {
-  // let imagen0 = imagen;
-  // console.log(imagen0);  
-  // }
-
-  // /* Contenedor de las imagenes, es un recorrido de la lista de imagenes que recibe del JSON */
-  // let IMAGES_CONTAINER = document.getElementsByClassName("container");
-  // for (let i = 0; i < currentProduct.images.length; i++) {
-  //   let imagen = currentProduct.images[i];
-  //   IMAGES_CONTAINER.innerHTML += ` 
-  //   <div id="imagen0"></div>`
-  //   let imagenCentral = document.getElementById("imagen0");
-  //   imagenCentral.innerHTML = `
-  //       <img src="${imagen[0]}" class="d-block w-100" alt="">`
-  // };
-
+  }
+  let imagenes = document.getElementsByClassName("carousel-item")
+  imagenes[0].className += " active"
+  //inserto los controles del slider
+  carouselContainer.innerHTML += `
+  <a class="carousel-control-prev" href="#carouselProducto" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselProducto" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>`
 }
 
-// imagenes[0].className += " active"
+function sortComentarios(array) {
+  array.sort(function (a, b) {
+    var dateA = new Date(a.dateTime).getTime();
+    var dateB = new Date(b.dateTime).getTime();
+    return dateA < dateB ? 1 : -1; // Investigar qué hace estructurar de esta forma!!!!!!!!!!
+  });
+  return array;
+}
 
-// IMAGES_CONTAINER.innerHTML += `
-// </div>
-// <a class="carousel-control-prev" href="#carouselProducto" role="button" data-slide="prev">
-//   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-//   <span class="sr-only">Previous</span>
-// </a>
-// <a class="carousel-control-next" href="#carouselProducto" role="button" data-slide="next">
-//   <span class="carousel-control-next-icon" aria-hidden="true"></span>
-//   <span class="sr-only">Next</span>
-// </a>
-// </div>`
+// REVISAR QUÉ PASA CON ESTE COMENTARIO QUE NO LO AGREGA
+// REVISAR DEL ARCHIVO DE CREAR UN NUEVO COMENTARIO HECHO EN CLASE SOLO QUE CAMBIANDO A OBJETO A IMPRIMIR EN HTML
 
-//     <h1 class="text-center">${currentProduct.name}</h1>
-//     <div class="col-6">
-//         <div id="carouselProducto" class="carousel slide" data-ride="carousel">
-//             <div class="carousel-inner">
-//             <span class="carousel-control-next-icon" aria-hidden="true"></span></div>
-//         </div>
-//     </div>
-//   <div class="col-6">
-//     <h2>${currentProduct.currency} ${currentProduct.cost}</h2>
-//     <p>${currentProduct.description}</p>
-//     </div>`
-//     // <button class="btn btn-primary">Agregar al carrito</button>
+// function newComentario() {
+//   let date = new Date();
+//   var newcommment = {
+//     score: document.getElementById("comentarios-stars").value,
+//     description: document.getElementById("comentarios").value,
+//     user: localStorage.getItem('usuario'),
+//     dateTime: date
+//   };
 
-//     let IMAGES_CONTAINER = document.getElementsByClassName("carousel-inner")[0];
-//     for (let i = 0; i < currentProduct.images.length; i++) {
-//         let imagen = currentProduct.images[i];
-//         IMAGES_CONTAINER.innerHTML += ` 
-//         <div class="carousel-item">
-//         <img src="${imagen}" class="d-block w-100" alt="">
-//       </div>
+FORMULARIO_COMENTARIOS.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let date = new Date();
+  
+  let newcomment = {
+    score: parseInt(document.querySelector('input[type="radio"]:checked').value),
+    description: document.getElementById("comentarios").value,
+    user: JSON.parse(localStorage.getItem('usuario'))[0].usuario,
+    dateTime: `${date.getFullYear()}-${(date.getMonth()+1 < 10) ? "0" : ""}${date.getMonth()}-${date.getDate() < 10 ? "0" : ""}${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+  };
+  COMENTARIOS.innerHTML = '';
+  comentariosLista.push(newcomment);
+  comentariosJSON();
+})
+
+function comentariosJSON() {
+  comentariosLista = sortComentarios(comentariosLista);
+  for (let i = 0; i < comentariosLista.length; i++) {
+    let comentarios = comentariosLista[i];
+
+    for (let i = 1; i <= 5; i++) {
+      if (i <= comentarios.score) {
+        COMENTARIOS.innerHTML += `<span class="fas fa-star checked"></span>`
+      } else {
+        COMENTARIOS.innerHTML += `<span class="fas fa-star"></span>`
+      }
+    }
+
+    COMENTARIOS.innerHTML += `<div class="row"><div class="col"><div class="d-flex w-100 justify-content-between">
+        <h6 class="font-weight-bold">${comentarios.user}</h6>
+        <small class="text-muted">${comentarios.dateTime}</small></div>
+        <p class="mb-3">${comentarios.description}</p></div></div>`
+  };
+  FORMULARIO_COMENTARIOS.reset();
+}
 
 
 document.addEventListener("DOMContentLoaded", function (e) {
   getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
     if (resultObj.status === "ok") {
       ShowProductInfo(resultObj.data);
+    }
+  });
+
+  getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      comentariosLista = resultObj.data;
+      comentariosJSON();
     }
   });
 });
