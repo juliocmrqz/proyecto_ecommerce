6e66d6b7e251981450807912398f25341cf19ef9
juliocmrqz <juliomarquez3093@gmail.com>
2021-10-14 20:46:54 -0300

    Carrito
    .

--- js/cart.js
+++ js/cart.js
@@ -2,17 +2,19 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 const CART_CONTAINER = document.getElementById("cart-container");
-const CART_SUBTOTAL = document.getElementById("cart-subtotal");
-const REMOVE_BUTTON = document.getElementById("cart-remove-button");
+const CART_SUBTOTAL_CONTAINER = document.getElementById("cart-subtotal");
 const CART_QUANTITY = document.getElementById("cart-quantity");
 const CURRENCY_EXCHANGE = 40;
+const CURRENCY = "UYU";
+const TOTAL_AFTER_SHIPPING = document.getElementById("total-after-shipping");
+const SHIPPING_COST = document.getElementById("shipping-cost");
 let cartProducts;
+let total = 0;
 
 /**
- * 
- * @param {*} id indice del valor que recibe
- * @param {*} cost costo del producto
- * @returns el valor de la moneda ya convertido
+ * Toma el indice del valor que recibe
+ * Toma el costo del producto
+ * Devuelve el valor de la moneda sea ya convertido o si está en pesos, devuelve ese mismo dato.
  */
 function currencyConverter(id, cost) {
   let currency = document.getElementById(`currencySubtotal${id}`).innerHTML;
@@ -24,38 +26,49 @@ function currencyConverter(id, cost) {
 }
 
 /**
- * 
+ * Inicializa el total a 0 y recorre la lista de articulos, considerando el subtotal y lo inserta ya convertido a pesos dentro del HTML
+ * Además muestra el valor total del carrito más el costo de envío.
+ * Considera el indice de cada producto más el costo ya que debe evaluar la moneda del producto para ver si hace o no la conversión a UYU
  */
 function showTotal() {
-  let total = 0;
+  total = 0;
   for (let i = 0; i < cartProducts.articles.length; i++) {
     let subtotal = parseFloat(document.getElementById(`subtotal${i}`).innerHTML);
     total += currencyConverter(i, subtotal);
   }
-  CART_SUBTOTAL.innerHTML = `UYU ${numberWithCommas(total.toFixed(2))}`;
+  CART_SUBTOTAL_CONTAINER.innerHTML = `UYU ${addCommasToNumbersOverAThousand(total.toFixed(2))}`;
+  totalCostAfterShippingMethod()
 }
 
 /**
- * TO DO
- * IMPLEMENTAR CUSTOM ATTRIBUTE DATA-UNITCOST PARA EL PRECIO
- * ASÍ NO ME METO AL INNERTEXT.
+ * Función que toma el indice para poder identificar cada valor a utilizar para el calculo de precio por cantidad para subtotal por producto
+ * Obtiene el valor en texto convertido a número del precio
+ * Obtiene el valor de la cantidad convertido a número.
+ * calcula el precio por cantidad y lo inserta dentro del HTML para cada artículo
  */
 function priceMultiplier(index) {
   let precio = parseInt(document.getElementById(`precio${index}`).innerText);
   let cantidad = parseInt(document.getElementById(`cantidad${index}`).value);
-  cartProducts.articles[index].count = cantidad;
-  let total = precio * cantidad;
-  document.getElementById(`subtotal${index}`).innerHTML = total;
+  // cartProducts.articles[index].count = cantidad;
+  let subtotal_individual = precio * cantidad;
+  document.getElementById(`subtotal${index}`).innerHTML = subtotal_individual;
   showTotal();
 }
-
+ /**
+  * Función para remover el articulo según el indice del producto que se le pase al botón con evento onclick.
+  * Remueve el articulo de la lista de articulos en el carrito, así solo debe mostrarse la lista nuevamente de artículos restantes.
+  * Adicional a esto sigue mostrando el restante del carrito  
+  */
 function removeCartItem(index) {
   cartProducts.articles.splice(index, 1);
   showCartInfo();
 }
 
 /**
- * 
+ * Muestro los datos recibidos del getJSONData recorriendo la lista que se guarda localmente de productos en el carrito
+ * Crea el HTML necesario para insertarlo en el contenedor del carrito de compra
+ * Toma la cuenta del total de articulos que están ingresados en el carrito
+ * Utiliza la función ShowTotal para poder mostrar el total del costo de los productos sin tener que esperar algún evento.
  */
 function showCartInfo() {
   let htmlContentToAppend = "";
@@ -87,11 +100,9 @@ function showCartInfo() {
         <h6><span id="currencySubtotal${i}">${productInCart.currency}</span><span id="subtotal${i}" class="cart_item_text"> ${productInCart.count * productInCart.unitCost}</span></h6>
     </td>
     <td>
-        <button href="#" class="btn btn-danger" onclick="removeCartItem(${i})"><i
-                class="fa fa-trash"></i> Remove item</button>
+        <button href="#" class="btn btn-danger" onclick="removeCartItem(${i})"><i class="fa fa-trash"></i></button>
     </td>
-</tr>
-<!----->`;
+</tr>`;
     cartQuantity++;
   }
   CART_CONTAINER.innerHTML = htmlContentToAppend;
@@ -99,6 +110,19 @@ function showCartInfo() {
   showTotal();
 }
 
+/**
+ * Toma el valor del select el cual solo guarda el porcentaje en número entero del costo de envío
+ * Agrega en el HTML el porcentaje de envío en HTML más texto de apoyo para el comprador, así no lo pasa en el select
+ * Calcula el valor total del carrito más costo de envío al estar seleccionado 1
+ * Inserta en el HTML el valor calculado
+ */
+function totalCostAfterShippingMethod() {
+  var shippingCostPercentage = document.getElementById("moneda").value;
+  let shippingCost = addCommasToNumbersOverAThousand(parseInt(total * (shippingCostPercentage / 100)).toFixed(2)) + "<br>" + `<small class="text-muted">(${shippingCostPercentage}% del subtotal)</small>`;
+  let totalCostToShow = CURRENCY + " " + addCommasToNumbersOverAThousand(parseFloat((Math.round(total * shippingCostPercentage) / 100) + total).toFixed(2));
+  TOTAL_AFTER_SHIPPING.innerHTML = totalCostToShow;
+  SHIPPING_COST.innerHTML = shippingCost;
+}
 
 document.addEventListener("DOMContentLoaded", function (e) {
   getJSONData(CART_INFO_URL).then(function (cartInfo) {
--- js/init.js
+++ js/init.js
@@ -8,7 +8,7 @@ const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/654.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 const NAVBAR_ALL = document.getElementById("navbar-all");
 
-function numberWithCommas(x) {
+function addCommasToNumbersOverAThousand(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
